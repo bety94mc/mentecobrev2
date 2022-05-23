df4330f8419774671d94cf2b3109493a4efc1b00
Sira <bety94mc@gmail.com>
2022-05-23 09:56:35 +0200

    'Enlaces que abren a nueva pestañas y log admin incluido'

--- mentecobre/admin.py
+++ mentecobre/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry, DELETION
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 from .models import Glosario, Usuario, Articulos
 from .forms import UsuarioActualizarForm, RegistroForm
 
-from import_export.admin import ImportExportModelAdmin, ExportActionMixin
+from import_export.admin import ImportExportModelAdmin, ExportActionMixin, ExportMixin
 from django_admin_listfilter_dropdown.filters import (
     DropdownFilter, ChoiceDropdownFilter, RelatedDropdownFilter, SimpleDropdownFilter
 )
-from django.urls import path
+from django.urls import path, reverse
 
 import csv
 from django.http import HttpResponse
@@ -23,6 +26,48 @@ class GlosarioResource(resources.ModelResource):
 
 
 # Register your models here.
+
+@admin.register(LogEntry)
+class LogEntryAdmin(ExportMixin, admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+    readonly_fields = [ 'user','content_type','object_id','object_repr','action_flag','change_message']
+    list_filter = ['user', 'content_type']
+    search_fields = ['object_repr', 'change_message']
+    list_display = ['__str__', 'content_type', 'action_time', 'user', 'object_link']
+
+    # keep only view permission
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = obj.object_repr
+        else:
+            ct = obj.content_type
+            try:
+                link = mark_safe('<a href="%s">%s</a>' % (
+                                 reverse('admin:%s_%s_change' % (ct.app_label, ct.model),
+                                         args=[obj.object_id]),
+                                 escape(obj.object_repr),
+                ))
+            except NoReverseMatch:
+                link = obj.object_repr
+        return link
+    object_link.admin_order_field = 'object_repr'
+    object_link.short_description = 'object'
+
+    def queryset(self, request):
+        return super(LogEntryAdmin, self).queryset(request) \
+            .prefetch_related('content_type')
+
+
+
 
 @admin.register(Usuario)
 class UsuarioAdmin(ImportExportModelAdmin, UserAdmin):
