68439bcc814e9727eca326293ada60eef0dcfa01
Sira <bety94mc@gmail.com>
2022-06-29 20:14:05 +0200

    'Incluir contadores de traducción y revisión'

--- mentecobre/static/js/funcion_ajax.js
+++ mentecobre/static/js/funcion_ajax.js
@@ -48,7 +48,18 @@ $(document).ready(function() {
 	});  
 	
     $('#gregoriomodal').modal({show:true});
-
+	
+	$('.counter-value').each(function(){
+			$(this).prop('Counter',0).animate({
+				Counter: $(this).text()
+			},{
+				duration: 3500,
+				easing: 'swing',
+				step: function (now){
+					$(this).text(Math.ceil(now));
+				}
+			});
+		});
 
   });
  
